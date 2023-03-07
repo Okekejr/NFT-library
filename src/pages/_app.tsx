f11a151c0ba53464b938be0a4912759d8ee72a9d
Emmanuel <85338878+Okekejr@users.noreply.github.com>
2023-03-07 00:15:55 -0600

    cleanUp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,11 @@
-import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { Layout } from "@/components/layout";
 import theme from "@/theme";
 
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
